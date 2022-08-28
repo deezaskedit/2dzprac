137972cd116430cf0ac4441c3f7a649e7aa6827c
Artem <aneday05@mail.ru>
2022-08-28 10:27:03 +0300

    13zd completed

--- 13zd/Program.cs
+++ 13zd/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void ThirdNumber(int number)
+{
+    if(number >= 100 ^ number <= -100)
+    {
+        int current = number;
+        int output = 0;
+        while(current > 1000 ^ current < -1000)
+        {
+            current /= 10;
+        }
+        output = current % 10;
+        if (output < 0) output = output * (-1);
+        Console.WriteLine($"Третья цифра введенного числа это {output}");
+
+    }
+    else
+    {
+        Console.WriteLine("Введите трехзначное число или больше");
+    }
+}
+Console.WriteLine("Введите число");
+int inputnum = Convert.ToInt32(Console.ReadLine());
+ThirdNumber(inputnum);
