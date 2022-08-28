36d5d333da421e7279c80f2bee75acfc4fa44ccd
Artem <aneday05@mail.ru>
2022-08-28 11:02:08 +0300

    15zd completed

--- 15zd/Program.cs
+++ 15zd/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void DayOff(int number)
+{
+    if(number == 1) Console.WriteLine($"Понедельник");
+    if(number == 2) Console.WriteLine($"Вторник");
+    if(number == 3) Console.WriteLine($"Среда");
+    if(number == 4) Console.WriteLine($"Четверг");
+    if(number == 5) Console.WriteLine($"Пятница");
+    if(number == 6) Console.WriteLine($"Суббота - выходной день!!!");
+    if(number == 7) Console.WriteLine($"Воскресенье - выходной день!!!");
+}
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+DayOff(num);
