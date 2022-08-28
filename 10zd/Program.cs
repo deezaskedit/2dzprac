7bb0b24735cb44ac9c52af0f446d5103946d1330
Artem <aneday05@mail.ru>
2022-08-28 10:14:46 +0300

    10zd completed

--- 10zd/Program.cs
+++ 10zd/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿//Задание 10
+int SecNumber (int number)
+{
+
+    int n1 = (number / 10) % 10;
+    return n1;
+}
+int randNumber = new Random().Next(100, 1000);
+int secnum = SecNumber(randNumber);
+
+Console.WriteLine($"Secnumber of {randNumber} is {secnum}");
